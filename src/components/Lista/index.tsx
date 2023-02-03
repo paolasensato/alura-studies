34cd3ce6fc13b005744091a99ab938e2ce2e4a28
paolasensato <sensatopaola@gmail.com>
2023-02-03 17:28:21 -0300

    Implementação do UseState no componente da Lista

--- src/components/Lista/index.tsx
+++ src/components/Lista/index.tsx
@@ -1,22 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Item from './Item';
 import style from './Lista.module.scss';
 
 function Lista() {
-    const tasks = [{
-        tarefa: 'React',
-        tempo: '02:00:00'
-    }, {
-        tarefa: 'JavaScript',
-        tempo: '02:30:00'
-    }, {
-        tarefa: 'TypeScript',
-        tempo: '03:00:00'
-    }];
+    const [ tasks, setTasks ] = useState([{
+            tarefa: 'React',
+            tempo: '02:00:00'
+        }, {
+            tarefa: 'JavaScript',
+            tempo: '02:30:00'
+        }, {
+            tarefa: 'TypeScript',
+            tempo: '03:00:00'
+        }]);
 
     return (
         <aside className={style.listaTarefas}>
-            <h2>Estudos do dia</h2>
+            <h2 onClick={() => {
+                setTasks([...tasks, { tarefa: "Estudar estado", tempo: "05:00:00"}])
+            }}>
+                Estudos do dia</h2>
             <ul>
                 {tasks.map((item, index) => (
                    <Item
